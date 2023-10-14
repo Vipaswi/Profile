f34f130db7d937ebf1221117fa40407b1a2d6b83
Vipaswi Thapa <vipa.thapa@gmail.com>
2023-10-14 00:02:44 -0400

    Fixed stars, removed some unecessary css

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,13 +5,13 @@ import logo from "public/assets/icon.png"
 
 const Navbar = () => {
   return (
-    <div className="navbar bg-gray-800 w-full flex-row flex justify-between align-middle border-b border-gray-400 px-5">
+    <div className="navbar w-full flex-row flex justify-between align-middle border-b border-gray-400 px-5">
       <div className="flex flex-row justify-center items-center ml-3 pl-3">
         <Image src={logo} width={70} height={60} alt="Icon"/>
       </div>
       <div className="flex flex-row text-gray-250 text-sm sm:text-xl m-8 space-x-3 mr-3">
         <Link href="/" className="">
-          <p className="navbar_text">About Me</p>
+          <p className="navbar_text ">About Me</p>
         </Link>
         <Link href="">
           <p className="navbar_text">My Interests</p>
